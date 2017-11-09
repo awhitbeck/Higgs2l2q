82f6e9a3d406b0cc774f9cc685d43b1d92b39667
awhitbeck <whitbeck.andrew@gmail.com>
2017-11-09 14:24:34 -0600

    first working version

--- TreeMaker/test/analysis_tree_cfg.py
+++ TreeMaker/test/analysis_tree_cfg.py
@@ -39,7 +39,7 @@ process.TFileService = cms.Service("TFileService",
 )
 
 process.genParticleDump = cms.EDProducer('GenParticlesProducer',
-                                         genCollection = cms.InputTag('genParticles'),
+                                         genCollection = cms.InputTag('prunedGenParticles'),
                                          debug = cms.bool(True),
                                          childIds = cms.vint32(1,2,3,4,5,11,12,13,14,15,16),
                                          parentIds = cms.vint32(1,2,6,23,24,25,))
@@ -63,8 +63,33 @@ process.TreeMaker = TreeMaker.clone(TreeName = cms.string('analysisTree'),
                                     VectorRecoCand = cms.vstring()
                                     )
 
-process.TreeMaker.VectorRecoCand.append("ak4GenJetsNoNu")
-process.TreeMaker.VectorRecoCand.append("ak8GenJetsNoNu")
+process.leptonproducer = cms.EDProducer('LeptonProducer',
+                                        MuonTag = cms.InputTag('slimmedMuons'),
+                                        ElectronTag = cms.InputTag('slimmedElectrons'),
+                                        PrimaryVertex = cms.InputTag('offlineSlimmedPrimaryVertices'),
+                                        minElecPt  = cms.double(5),
+                                        maxElecEta  = cms.double(2.5),
+                                        minMuPt  = cms.double(5),
+                                        maxMuEta  = cms.double(2.4),
+                                        muIsoValue  = cms.double(0.2),
+                                        elecIsoValue  = cms.double(0.2), # only has an effect with useMiniIsolation
+                                        UseMiniIsolation = cms.bool(False),
+                                        METTag = cms.InputTag('slimmedMETs'), 
+                                        rhoCollection = cms.InputTag("fixedGridRhoFastjetAll")
+                                        )
+
+process.TreeMaker.VectorBool.append("leptonproducer:IdMuonTightID(muon_tightID)")
+process.TreeMaker.VectorBool.append("leptonproducer:IdIsoMuonTightID(muon_tightIDIso)")
+process.TreeMaker.VectorInt.append("leptonproducer:MuonCharge(muon_charge)")
+process.TreeMaker.VectorRecoCand.append("leptonproducer:IdMuon(muons)")
+process.TreeMaker.VectorBool.append("leptonproducer:IdElectronTightID(electron_tightID)")
+process.TreeMaker.VectorBool.append("leptonproducer:IdIsoElectronTightID(electron_tightIDIso)")
+process.TreeMaker.VectorInt.append("leptonproducer:ElectronCharge(electron_charge)")
+process.TreeMaker.VectorRecoCand.append("leptonproducer:IdElectron(electrons)")
+process.TreeMaker.VectorRecoCand.append("slimmedJets")
+#process.TreeMaker.VectorRecoCand.append("slimmedElectrons")
+#process.TreeMaker.VectorRecoCand.append("slimmedMuons")
+process.TreeMaker.VectorRecoCand.append("slimmedJetsAK8")
 
 process.TreeMaker.VectorTLorentzVector.append("genParticleDump(GenParticles)")
 process.TreeMaker.VectorInt.append("genParticleDump:PdgId(GenParticles_PdgId)")
@@ -72,8 +97,11 @@ process.TreeMaker.VectorInt.append("genParticleDump:Status(GenParticles_Status)"
 process.TreeMaker.VectorInt.append("genParticleDump:Parent(GenParticles_ParentIdx)")
 process.TreeMaker.VectorInt.append("genParticleDump:ParentId(GenParticles_ParentId)")
 
-process.p = cms.Path(process.genJetParticles + process.ak5GenJets 
-                     + process.genParticleDump + process.TreeMaker)
+process.p = cms.Path(#process.genJetParticles +  
+                     #process.ak4GenJets + 
+                     process.genParticleDump + 
+                     process.leptonproducer +
+                     process.TreeMaker)
 
 
 process.schedule = cms.Schedule(
